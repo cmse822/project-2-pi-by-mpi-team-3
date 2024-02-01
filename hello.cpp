2e1bffef3a584c8c51f60236fd4d8b49fad7ca97
kenzerkay <mscott3327@gmail.com>
2024-02-01 11:41:54 -0500

    Push Classwork from 2/1/2023

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include "mpi.h"
+#include <stdio.h> 
+#include <stdlib.h>
+
 using namespace std;
 
-int main() 
-{
+
+// Still not initaized right
+
+int main(int argc,char *argv[]) {
+
+    int numtasks, rank;
     cout << "Hello, World!" << endl;
+
+    //MPI_Status Stat;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    cout << "Hello, World!" << endl;
+
+    MPI_Finalize();
+
+    cout << "Hello, World!" << endl;
+
     return 0;
 }
