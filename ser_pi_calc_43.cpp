1fa8c6d554f82ece6ce1509eb243d3387a080650
Sunia Tanweer <114263384+stanweer1@users.noreply.github.com>
2024-02-09 16:29:26 -0500

    Adding code and exe for 4.3

---
+++ ser_pi_calc_43.cpp
@@ -0,0 +1,126 @@
+/***************************************************************************
+ * FILE: ser_pi_calc.c
+ * DESCRIPTION:  
+ *   Serial pi Calculation - C Version
+ *   This program calculates pi using a "dartboard" algorithm.  See
+ *   Fox et al.(1988) Solving Problems on Concurrent Processors, vol.1
+ *   page 207.  
+ * AUTHOR: unknown
+ * REVISED: 02/23/12 Blaise Barney
+ * 
+ * Sunia: Made edits to divide ROUNDS in ranks and use MPI_REDUCE to 
+ * average the averages of pi from each rank. 
+***************************************************************************/
+#include <iostream> 
+#include <stdio.h>
+#include <stdlib.h>
+#include "mpi.h" 
+
+using namespace std; 
+
+void srandom (unsigned seed);  
+double dboard (int darts);
+
+#define DARTS 10000   	/* number of throws at dartboard */
+#define ROUNDS 100    	/* number of times "darts" is iterated */
+
+int main(int argc, char *argv[]){
+
+int numtasks, rank; 
+double start_time, end_time, elapsed, max_elapsed; 
+
+double pi;          	/* average of pi after "darts" is thrown */
+double avepi, sum_all_pi;       	/* average pi value for all iterations */
+int i, n;
+
+MPI_Init(&argc, &argv); 
+MPI_Comm_size(MPI_COMM_WORLD, &numtasks); 
+MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+int count = ROUNDS / numtasks + ((ROUNDS % numtasks) > rank);
+
+printf("Starting pi calculation using dartboard algorithm...\n");
+srandom (rank);            /* seed the random number generator */
+avepi = 0;
+
+MPI_Barrier(MPI_COMM_WORLD);
+start_time = MPI_Wtime();
+
+for (i = 0; i < count; i++) {
+   /* Perform pi calculation on serial processor */
+   pi = dboard(DARTS);
+   avepi = ((avepi * i) + pi)/(i + 1); 
+   printf("   After %3d throws, average value of pi = %10.8f, performed by rank %3d \n",
+         (DARTS * (i + 1)),avepi,rank);
+   }    
+   
+end_time = MPI_Wtime();
+elapsed = end_time - start_time;
+
+MPI_Reduce(&elapsed, &max_elapsed, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD); 
+MPI_Reduce(&avepi, &sum_all_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+
+if (rank == 0) {
+    printf("Calculated pi = %f \n", sum_all_pi / numtasks);
+    printf("Elapsed time = %f", max_elapsed);
+    printf("\nReal value of PI: 3.1415926535897 \n");
+}
+
+
+MPI_Finalize(); 
+}
+
+
+/*****************************************************************************
+ * dboard
+ *****************************************************************************/
+#define sqr(x)	((x)*(x))
+long random(void);
+
+double dboard(int darts)
+{
+   double x_coord,       /* x coordinate, between -1 and 1  */
+          y_coord,       /* y coordinate, between -1 and 1  */
+          pi,            /* pi  */
+          r;             /* random number scaled between 0 and 1  */
+   int score,            /* number of darts that hit circle */
+       n;
+   unsigned int cconst;  /* must be 4-bytes in size */
+/*************************************************************************
+ * The cconst variable must be 4 bytes. We check this and bail if it is
+ * not the right size
+ ************************************************************************/
+   if (sizeof(cconst) != 4) {
+      printf("Wrong data size for cconst variable in dboard routine!\n");
+      printf("See comments in source file. Quitting.\n");
+      exit(1);
+      }
+   /* 2 bit shifted to MAX_RAND later used to scale random number between 0 and 1 */
+   cconst = 2 << (31 - 1);
+   score = 0;
+
+/***********************************************************************
+ * Throw darts at board.  Done by generating random numbers
+ * between 0 and 1 and converting them to values for x and y
+ * coordinates and then testing to see if they "land" in
+ * the circle."  If so, score is incremented.  After throwing the
+ * specified number of darts, pi is calculated.  The computed value
+ * of pi is returned as the value of this function, dboard.
+ ************************************************************************/
+
+   for (n = 1; n <= darts; n++) {
+      /* generate random numbers for x and y coordinates */
+      r = (double)random()/cconst;
+      x_coord = (2.0 * r) - 1.0;
+      r = (double)random()/cconst;
+      y_coord = (2.0 * r) - 1.0;
+
+      /* if dart lands in circle, increment score */
+      if ((sqr(x_coord) + sqr(y_coord)) <= 1.0)
+         score++;
+      }
+
+   /* calculate pi */
+   pi = 4.0 * (double)score/(double)darts;
+   return(pi);
+} 
